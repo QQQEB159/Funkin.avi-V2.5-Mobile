565c9b9ab40dcb9e54f0f0fb8fe9489bfcbb6751
QQQEB159 <2724758997@qq.com>
2026-08-20 08:57:35 +0800

    huh

--- source/Init.hx
+++ source/Init.hx
@@ -50,7 +50,7 @@ class Init extends FlxState
 		FlxG.game.focusLostFramerate = 60;
 		FlxG.keys.preventDefaultKeys = [TAB];
 
-        FlxG.sound.soundTray.silent = true; // removes that annoying ass "BEEP" sound when you change the volume
+        //FlxG.sound.soundTray.silent = true; // removes that annoying ass "BEEP" sound when you change the volume
 
         #if DISCORD_ALLOWED
 		DiscordClient.prepare();
